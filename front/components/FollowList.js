1b7189212f17fcb5da29117047496a5af67997e8
sungmin-choi <namja306@naver.com>
2021-12-23 15:15:43 +0900

    Feat) Followlist , NicknameEditForm 구현완료

--- front/components/FollowList.js
+++ front/components/FollowList.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { Card, Col, Row,Button } from 'antd';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import {StopTwoTone} from '@ant-design/icons';
+    
+const ScCard = styled(Card)`
+    margin-top: 2rem;
+    text-align: center;
+`;
+const ScButton = styled(Button)`
+    margin-top: 1rem;
+`
+const ScStopTwoTone = styled(StopTwoTone)`
+    font-size: 1.5rem;
+`;
 
-const FollowList = props => {
+const FollowList = ({name,data}) => {
     return (
-        <div>
-            
-        </div>
+        <ScCard
+        title={name}
+        >
+        <Row gutter={10}>
+            {data&& data.map(ele=>{
+                return (<Col span={6}>
+                            <Card title={ele.nickname}>
+                                <ScStopTwoTone />
+                            </Card>
+                        </Col>);
+            })}
+        </Row>
+            <ScButton>더 보기</ScButton>
+        </ScCard>
     )
 }
 
