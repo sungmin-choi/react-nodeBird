a3e9df332346cf7eb3943caface06edd1b5a9ae2
sungmin-choi <namja306@naver.com>
2021-12-31 13:18:48 +0900

    Feat) redux-saga 추가 및 설정 sagas폴더 생성

--- front/store/congiureStore.js
+++ front/store/congiureStore.js
@@ -1,14 +1,22 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore } from "redux";
 import {composeWithDevTools} from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 import reducer from '../reducers';
+import rootSaga from '../sagas';
 
 const configureStore = () =>{
-    const middlewares = [];
+    const sagaMiddleware = createSagaMiddleware();
+
+    const middlewares = [sagaMiddleware];
+
     const enhancer = process.env.NODE_ENV === 'production' ?
     compose(applyMiddleware(...middlewares)): 
     composeWithDevTools(applyMiddleware(...middlewares));
     const store = createStore(reducer,enhancer);
+
+    store.sagaTask = sagaMiddleware.run(rootSaga);
+
     return store;
 };
 
